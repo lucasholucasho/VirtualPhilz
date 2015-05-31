5f13095aae9d104023ab36747dfa9c87612a7ecb
Lucas Ho <lho@princeton.edu>
2015-05-30 22:10:29 -0700

    Cube click displays text

---
+++ New Unity Project/Assets/Cardboard/DemoScene/Teleport.cs
@@ -0,0 +1,55 @@
+﻿// Copyright 2014 Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof(Collider))]
+public class Teleport : MonoBehaviour {
+  private Vector3 startingPosition;
+  private bool showText = false;
+
+  void Start() {
+    startingPosition = transform.localPosition;
+    SetGazedAt(false);
+  }
+
+  public void SetGazedAt(bool gazedAt) {
+    GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+  }
+
+  public void Reset() {
+    transform.localPosition = startingPosition;
+  }
+
+  public void ToggleVRMode() {
+    Cardboard.SDK.VRModeEnabled = !Cardboard.SDK.VRModeEnabled;
+  }
+
+  public void TeleportRandomly() {
+		showText = true;
+  }
+
+	void OnGUI()
+	{
+		if(showText)
+		{
+			// If you've clicked the object, show this button
+			if(GUI.Button(new Rect(100,100,100,20), "I love Philz"))
+				// If you click this button, set showText to false
+				showText = false;
+		}
+	}
+}
